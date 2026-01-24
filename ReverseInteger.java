472fa5f1afac5f76c56ec2770c9a9186037ea52f
NagendraM18 <nagendramgowda2124@gmail.com>
2026-01-24 11:47:59 +0530

    sloved reverse Integer problem  from leetcode

---
+++ ReverseInteger.java
@@ -0,0 +1,31 @@
+
+class Solution {
+    public int reverse(int x)
+    {
+        int sum=0;
+      while(x!=0 )
+      {
+        
+        int d=x%10;
+        x=x/10;
+        if(sum >(Integer.MAX_VALUE/10) || sum == Integer.MAX_VALUE/10 &&   d >7) 
+            return 0;
+        if(sum<Integer.MIN_VALUE/10 || sum == Integer.MIN_VALUE/10 && d <-8)
+            return 0;
+
+        sum=sum*10+d;
+      }
+        return sum;
+    }
+}
+
+public class ReverseInteger
+{
+    public static void main(String arg[])
+    {
+        Solution obj=new Solution();
+        int X=-123;
+        int result=obj.reverse(X);
+        System.out.println(result);
+    }
+}
